c01d3b7fa0d8affde61d71388a564124cc8a6b11
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-10-02 04:46:06 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { useState } from 'react';
-import Form from './components/Form';
-import Navbar from './components/Navbar';
+import { useState } from "react";
+import Form from "./components/Form";
+import Navbar from "./components/Navbar";
 
 function App() {
 	const [darkTheme, setDarkTheme] = useState(false);
 	return (
 		<>
 			<div className="bg-backg-light dark:bg-backg-dark">
-				<div className={darkTheme ? 'dark' : ''}>
+				<div className={darkTheme ? "dark" : ""}>
+					HI THERE
 					<Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
 					<Form />
 				</div>
